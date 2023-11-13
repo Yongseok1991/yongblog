9868eb988e6dec936aa734affea2e0e77a84a0c8
yongseok1991 <dobest09998@gmail.com>
2023-11-13 09:29:54 +0900

    first init

---
+++ src/main/java/my/yongblog/YongblogApplication.java
@@ -0,0 +1,13 @@
+package my.yongblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YongblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YongblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/yongblog/YongblogApplicationTests.java
@@ -0,0 +1,13 @@
+package my.yongblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YongblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
